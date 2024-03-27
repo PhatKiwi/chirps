37cda15d6beddb6ab9ccfe08fdb96512d345b3da
Ethan Anderson <ethanandy90@gmail.com>
2024-03-27 12:13:12 -0600

    install active storage and display user avatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :chirps, dependent: :destroy
+  has_one_attached :avatar
 
   validates :username, uniqueness: { case_sensitive: false}, allow_blank: true
 
